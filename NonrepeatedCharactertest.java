85849b897c4e4270b512d9fc69b3dca4d59270b7
palugunak <39412446+palugunak@users.noreply.github.com>
2023-05-26 18:04:41 -0400

    updatd hash programs

---
+++ NonrepeatedCharactertest.java
@@ -0,0 +1,16 @@
+public class NonrepeatedCharactertest {
+
+    public static void main(String[] args){
+
+        character cha =new character();
+     //   Character chay = cha.nonrepeatedChar("green apple");
+        Character chayE = cha.lastnonrepeatedChar("GeeksForGeeks");
+
+      //  System.out.println(chay);
+        System.out.println(chayE);
+
+
+    }
+
+
+}
--- character.java
+++ character.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,15 +9,78 @@ public class character {
 
     Set<Character> set = new HashSet<>();
 
-    public  char findfirstrepeatedChar(String str) {
-          for(Character ch: str.toCharArray()){
+    public char findfirstrepeatedChar(String str) {
+        for (Character ch : str.toCharArray()) {
 
-              if(set.contains(ch))
-                  return ch;
+            if (set.contains(ch))
+                return ch;
 
-              set.add(ch);
-          }
+            set.add(ch);
+        }
 
-          return Character.MIN_VALUE;
+        return Character.MIN_VALUE;
+    }
+
+
+    public char nonrepeatedChar(String str) {
+
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        // lets us check first we have character available in hashmap
+        // if available count the how many time the char is there
+
+
+        for (Character cha : str.toCharArray()) {
+
+            if (map.containsKey(cha)) {
+
+                int count = map.get(cha);
+                map.put(cha, count + 1);
+
+            } else {
+                map.put(cha, 1);
+            }
+
+
+            for (Character chu : str.toCharArray()) {
+
+                if (map.get(chu) == 1)
+
+                    return chu;
+            }
+
+
+        }
+        return Character.MIN_VALUE;
+
+    }
+
+    public char lastnonrepeatedChar(String str) {
+        int n = str.length();
+        HashMap<Character, Integer> hasmap = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+
+            if (hasmap.containsKey(str.charAt(i))) {
+                int count = hasmap.get(str.charAt(i));
+                hasmap.put(str.charAt(i), count + 1);
+
+            } else {
+                hasmap.put(str.charAt(i), 1);
+            }
+
+        }
+
+
+        for (int j = hasmap.size(); j >0;j--) {
+
+            if (hasmap.get(str.charAt(j)) == 1) {
+
+                return str.charAt(j);
+            }
+
+
+        }
+        return Character.MIN_VALUE;
+
     }
 }
