e394a8c7c7c6d9d544753aa6faa2dd0698389872
palugunak <39412446+palugunak@users.noreply.github.com>
2023-06-03 16:09:09 -0400

    kth non repating character]

---
+++ KthNonrepeatingCharacter.java
@@ -0,0 +1,101 @@
+import java.util.HashMap;
+import java.util.Map;
+/*
+public class KthNonrepeatingCharacter {
+
+
+    public static void main(String[] args) {
+
+        String str = "geeksforgeeks";
+        int k = 3;
+
+        System.out.println(kthnonrepeatingcharaater(str, k));
+
+
+    }
+
+    public static char kthnonrepeatingcharaater(String str, int k) {
+
+
+        HashMap<Character, Integer> hashMap = new HashMap<>();
+
+        /*  for (int i = 0; i < str.length(); i++) {
+
+            if (hashMap.containsKey(str.charAt(i))) {
+
+                int count = hashMap.get(str.charAt(i));
+                hashMap.put(str.charAt(i), count + 1);
+
+            } else
+
+                hashMap.put(str.charAt(i), 1);
+
+        }
+
+
+        for( char c :str.toCharArray() ){
+
+            if (hashMap.containsKey(c)) {
+
+                int count = hashMap.get(c);
+                hashMap.put(c, count + 1);
+
+            } else
+
+                hashMap.put(c, 1);
+
+
+
+
+        }
+
+
+        for(char charc : str.toCharArray())  {
+
+         if  (hashMap.containsValue(1)) {
+
+             k--;
+             if(k ==0){
+
+                 ;
+             }
+         }
+
+
+
+        }
+
+
+
+
+     /*   Character key = null;
+        for (Map.Entry<Character, Integer> item : hashMap.entrySet()) {
+
+            if (item.getValue() == 1) {
+
+                k--;
+                if (k == 0) {
+                    //   System.out.println(  "The Kth Non-repeating Character is :");
+                    //   System.out.println(item.getKey());
+                    key = item.getKey();
+                    break;
+                }
+
+
+            }
+
+
+        }
+
+        return key;
+    }
+}
+
+
+*/
+
+
+
+
+
+
